6efc9f585b4ade966c5f1c6ec08e4667f0027af8
shelen ells <shelen.ells@gmail.com>
2022-04-20 17:48:37 -0700

    all_please

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,8 +1,37 @@
 from django.contrib import admin
-from recipes.models import Recipe
+from recipes.models import Ingredient, Measure, FoodItem, Step, Recipe
 
 # Register your models here.
 class RecipeAdmin(admin.ModelAdmin):
-
     pass
+
+
 admin.site.register(Recipe, RecipeAdmin)
+
+
+class MeasureAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Measure, MeasureAdmin)
+
+
+class FoodItemAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(FoodItem, FoodItemAdmin)
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Ingredient, IngredientAdmin)
+
+
+class StepAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Step, StepAdmin)
--- recipes/models.py
+++ recipes/models.py
@@ -1,15 +1,58 @@
 from django.db import models
-
+from django.forms import DateTimeField
 
 # Create your models here.
 
+
 class Recipe(models.Model):
     name = models.CharField(max_length=125)
-    author =  models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
     description = models.TextField()
     image = models.URLField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.author
+
+
+class Measure(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    abreviation = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return self.author
+
+
+class FoodItem(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.author
+
+
+class Ingredient(models.Model):
+    amount = models.FloatField()
+    recipe = models.ForeignKey(
+        "Recipe", related_name="ingredients", on_delete=models.CASCADE
+    )
+    measure = models.ForeignKey(
+        "Measure", on_delete=models.PROTECT
+    )
+    food = models.ForeignKey(
+        "FoodItem", on_delete=models.PROTECT
+    )
+
+    def __str__(self):
+        return self.author
+
+
+class Step(models.Model):
+    recipe = models.ForeignKey(
+        "Recipe", related_name="Steps", on_delete=models.CASCADE
+    )
+    order = models.PositiveSmallIntegerField(())
+    directions = models.CharField(max_length=300)
 
     def __str__(self):
         return self.author
