f3d876b20c0084c291c8d4a86184478b8f6e5537
shelen ells <shelen.ells@gmail.com>
2022-04-21 12:56:50 -0700

    adding ratings correctly

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-
-from recipes.models import Recipe, Measure, FoodItem, Ingredient, Step
+from recipes.models import Recipe, Measure, FoodItem, Ingredient, Step, Rating
 
 
 # Register your models here.
@@ -24,8 +23,13 @@ class StepAdmin(admin.ModelAdmin):
     pass
 
 
+class RatingAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Measure, MeasureAdmin)
 admin.site.register(FoodItem, FoodItemAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Step, StepAdmin)
+admin.site.register(Rating, RatingAdmin)
