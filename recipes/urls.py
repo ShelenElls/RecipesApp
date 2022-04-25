bc3f6ef2239928e016e00c3cfeeefbf1f971efc7
shelen ells <shelen.ells@gmail.com>
2022-04-25 11:28:46 -0700

    converted list funciton to list class

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path
 
 from recipes.views import (
+    RecipeListView,
     create_recipe,
     change_recipe,
     log_rating,
     show_recipe,
-    show_recipes,
+    RecipeListView,
 )
 
+
 urlpatterns = [
-    path("", show_recipes, name="recipes_list"),
+    path("", RecipeListView.as_view(), name="recipes_list"),
     path("<int:pk>/", show_recipe, name="recipe_detail"),
     path("new/", create_recipe, name="recipe_new"),
     path("edit/", change_recipe, name="recipe_edit"),
