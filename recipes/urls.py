226b17d0b17831c8467a9b6606fa5af4475f159a
shelen ells <shelen.ells@gmail.com>
2022-04-25 15:40:32 -0700

    converted delete function

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from recipes.views import (
     RecipeListView,
     RecipeCreateView,
-    change_recipe,
+    RecipeUpdateVeiw,
+    RecipeDeleteView,
     log_rating,
     RecipeDetailView,
 )
@@ -13,6 +14,6 @@ urlpatterns = [
     path("", RecipeListView.as_view(), name="recipes_list"),
     path("<int:pk>/", RecipeDetailView.as_view(), name="recipe_detail"),
     path("new/", RecipeCreateView.as_view(), name="recipe_new"),
-    path("edit/", change_recipe, name="recipe_edit"),
+    path("<int:pk>/edit/", RecipeUpdateVeiw.as_view(), name="recipe_edit"),
     path("<int:recipe_id>/ratings/", log_rating, name="recipe_rating"),
 ]
