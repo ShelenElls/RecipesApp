dfaab7b23643c3844f01879180843ded655a8cc4
shelen ells <shelen.ells@gmail.com>
2022-04-25 12:13:14 -0700

    converted the create view for a Recipe

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,18 +2,17 @@ from django.urls import path
 
 from recipes.views import (
     RecipeListView,
-    create_recipe,
+    RecipeCreateView,
     change_recipe,
     log_rating,
     RecipeDetailView,
-    RecipeListView,
 )
 
 
 urlpatterns = [
     path("", RecipeListView.as_view(), name="recipes_list"),
     path("<int:pk>/", RecipeDetailView.as_view(), name="recipe_detail"),
-    path("new/", create_recipe, name="recipe_new"),
+    path("new/", RecipeCreateView.as_view(), name="recipe_new"),
     path("edit/", change_recipe, name="recipe_edit"),
     path("<int:recipe_id>/ratings/", log_rating, name="recipe_rating"),
 ]
