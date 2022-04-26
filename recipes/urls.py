8b2556b53b3cb74d59ca3cb138f6b5c6e710acda
shelen ells <shelen.ells@gmail.com>
2022-04-26 11:04:24 -0700

    updated zip file

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,20 +1,19 @@
 from django.urls import path
 
 from recipes.views import (
-    RecipeListView,
     RecipeCreateView,
-    RecipeUpdateView,
     RecipeDeleteView,
+    RecipeUpdateView,
     log_rating,
     RecipeDetailView,
+    RecipeListView,
 )
-
 
 urlpatterns = [
     path("", RecipeListView.as_view(), name="recipes_list"),
     path("<int:pk>/", RecipeDetailView.as_view(), name="recipe_detail"),
+    path("<int:pk>/delete/", RecipeDeleteView.as_view(), name="recipe_delete"),
     path("new/", RecipeCreateView.as_view(), name="recipe_new"),
     path("<int:pk>/edit/", RecipeUpdateView.as_view(), name="recipe_edit"),
-    path("<int:pk>/delete", RecipeDeleteView.as_view(), name="recipe_delete"),
     path("<int:recipe_id>/ratings/", log_rating, name="recipe_rating"),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
-from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from recipes.forms import RatingForm
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views.generic.list import ListView
 
+from recipes.forms import RatingForm
 
 try:
     from recipes.forms import RecipeForm
@@ -12,20 +12,6 @@ try:
 except Exception:
     RecipeForm = None
     Recipe = None
-
-
-class RecipeCreateView(CreateView):
-    model = Recipe
-    template_name = "recipes/new.html"
-    fields = ["name", "author", "description", "image"]
-    success_url = reverse_lazy("recipes_list")
-
-
-class RecipeUpdateView(UpdateView):
-    model = Recipe
-    template_name = "recipes/edit.html"
-    fields = ["name", "author", "description", "image"]
-    success_url = reverse_lazy("recipes_list")
 
 
 def log_rating(request, recipe_id):
@@ -41,7 +27,7 @@ def log_rating(request, recipe_id):
 class RecipeListView(ListView):
     model = Recipe
     template_name = "recipes/list.html"
-    paginate_by = 3
+    paginate_by = 2
 
 
 class RecipeDetailView(DetailView):
@@ -52,6 +38,20 @@ class RecipeDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context["rating_form"] = RatingForm()
         return context
+
+
+class RecipeCreateView(CreateView):
+    model = Recipe
+    template_name = "recipes/new.html"
+    fields = ["name", "author", "description", "image"]
+    success_url = reverse_lazy("recipes_list")
+
+
+class RecipeUpdateView(UpdateView):
+    model = Recipe
+    template_name = "recipes/edit.html"
+    fields = ["name", "author", "description", "image"]
+    success_url = reverse_lazy("recipes_list")
 
 
 class RecipeDeleteView(DeleteView):
